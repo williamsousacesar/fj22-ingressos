ba4e88cc2d8d8b38a41e2a5b8d0b9bb1ca6b6f78
William <william.cesar@gmail.com>
2019-04-06 09:35:17 -0300

    Validação de horario conflitantes entre as sessões. Utilização de testes.

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,7 @@ import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.form.SessaoForm;
+import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
 
 @Controller
 public class SessaoController {
@@ -43,14 +46,29 @@ public class SessaoController {
 
 	}
 
-	@PostMapping(value = "/admin/sessao")
+	//@PostMapping(value = "/admin/sessao")
+	//@Transactional
+	//public ModelAndView salva(@Valid SessaoForm form, BindingResult result) {
+		//if (result.hasErrors())
+			//return form(form.getSalaId(), form);
+		//Sessao sessao = form.toSessao(salaDao, filmeDao);
+		//sessaoDao.save(sessao);
+		//return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+	//}
+
+	@PostMapping("/admin/sessao")
 	@Transactional
 	public ModelAndView salva(@Valid SessaoForm form, BindingResult result) {
 		if (result.hasErrors())
 			return form(form.getSalaId(), form);
 		Sessao sessao = form.toSessao(salaDao, filmeDao);
-		sessaoDao.save(sessao);
-		return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+		List<Sessao> sessoesDaSala = sessaoDao.buscaSessoesDaSala(sessao.getSala());
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoesDaSala);
+		if (gerenciador.cabe(sessao)) {
+			sessaoDao.save(sessao);
+			return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+		}
+		return form(form.getSalaId(), form);
 	}
 
 }
